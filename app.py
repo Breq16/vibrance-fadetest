3b20ba8d2766077db239697045f8853ed783b619
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-08-17 15:46:52 -0400

    Add a web server to hopefully make Heroku happy

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+import threading
 
+from flask import Flask, redirect
 import vibrance
 
 manager = vibrance.Manager()
@@ -8,4 +10,17 @@ manager.chooseScript(manager.scripts["Fadetest"])
 
 manager.connect(os.environ.get("RELAY_IP"), os.environ.get("RELAY_PSK"))
 
-manager.run()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect("https://breq.dev/demos/vibrance/landing.html"
+                    "?host=vibrance.breq.dev")
+
+
+managerThread = threading.Thread(target=manager.run)
+managerThread.start()
+
+if __name__ == "__main__":
+    app.run()
