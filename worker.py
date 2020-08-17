5fa87f392256d676392466a44f2d6132cac366aa
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-08-17 16:14:47 -0400

    Re-enable worker process

--- worker.py
+++ worker.py
@@ -6,6 +6,7 @@ manager = vibrance.Manager()
 manager.addScript("fadetest.py")
 manager.chooseScript(manager.scripts["Fadetest"])
 
-manager.connect(os.environ.get("RELAY_IP"), os.environ.get("RELAY_PSK"))
+manager.connect(os.environ.get("VIBRANCE_RELAY_IP"),
+                os.environ.get("VIBRANCE_RELAY_PSK"))
 
 manager.run()
