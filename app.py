e1d1a33bab53947b85259395397fec81ac83fab9
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-08-17 16:03:17 -0400

    Separate worker and web processes for heroku

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-import os
-import threading
-import logging
-
 from flask import Flask, redirect
-import vibrance
-
-manager = vibrance.Manager()
-manager.addScript("fadetest.py")
-manager.chooseScript(manager.scripts["Fadetest"])
-
-manager.connect(os.environ.get("RELAY_IP"), os.environ.get("RELAY_PSK"))
-logging.getLogger("vibrance.controller.tolerant_socket").setLevel(logging.INFO)
 
 app = Flask(__name__)
 
@@ -20,9 +8,6 @@ def index():
     return redirect("https://breq.dev/demos/vibrance/landing.html"
                     "?host=vibrance.breq.dev")
 
-
-managerThread = threading.Thread(target=manager.run)
-managerThread.start()
 
 if __name__ == "__main__":
     app.run()
